be046b92f820eaee24cfced718e48fe38f660651
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-03-26 22:05:22 +0200

    Add css and jpg to webpack.config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import home from "./home.js";
 import menu from "./menu.js";
 import about from "./about.js";
+import "./styles.css";
 
 document.querySelector(".home").addEventListener("click", home);
 document.querySelector(".menu").addEventListener("click", menu);
